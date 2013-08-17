35fffd26d5bc5dc72e284306c74174eea04ce983
UENISHI Kota <kota@basho.com>
2013-08-17 15:04:35 +0900

    moved from msgpack-rpc/ruby

---
+++ lib/msgpack/rpc/version.rb
@@ -0,0 +1,7 @@
+module MessagePack
+module RPC
+
+VERSION = '0.5.1'
+
+end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+begin
+require 'rubygems'
+rescue LoadError
+end
+require 'test/unit'
+$LOAD_PATH.unshift File.dirname(__FILE__)+'/../lib'
+require 'msgpack/rpc'
