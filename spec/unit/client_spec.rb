4b72d60107c207d2e9780e24e03d23f632d7fe81
joker1007 <kakyoin.hierophant@gmail.com>
2015-01-06 17:14:43 +0900

    Fix client_spec
    Add wait for complete async process

--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -24,7 +24,7 @@ describe 'MessagePack::RPC::Client test' do
 	 
 	 it 'should return "ok" and "3" when you call with call_async' do
 	    req1 = @client.call_async(:hello)
-	    req2 = @client.call_async(:sum,1,2)
+	    req2 = @client.call_async(:sum, 1, 2)
 	    req1.join
 	    req1.result.should include("ok")
 	    req1.error.should be_nil
@@ -35,23 +35,26 @@ describe 'MessagePack::RPC::Client test' do
 	 end
 
 	 it 'should return "ok" when you set callback(:hello)' do
-	    @client.callback(:hello) do |error, result|
+	    req = @client.callback(:hello) do |error, result|
 	    	result.should include("ok")
 		error.should be_nil
 	    end
+	    req.join
 	 end
 
 	 it 'should return "3" when you set callback(:sum)' do
 
-	    @client.callback(:sum) do |error, result|
-	    	result.shouble equal 3
+	    req = @client.callback(:sum, 1, 2) do |error, result|
+	    	result.should equal 3
 		error.should be_nil
 	    end
+	    req.join
 	 end
 
 	 it 'should return nil values when you call notify' do
 	    @client.notify(:hello).should be_nil
 	    @client.notify(:sum,1,2).should be_nil
+      sleep 1
 	 end
 
 	 it 'should return error when you call private method' do
